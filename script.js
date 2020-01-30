86182d52efb6cad803d1b98ef4367da1b6bde410
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-30 16:29:39 +0100

    Refactoring 2

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 $(document).ready(function () {
 
-
   $(document).on('click', '.quadrato', function() {
-
     var quadrato = $(this);
 
     $.ajax({
-        'url' : "https://flynn.boolean.careers/exercises/api/random/int",
-        'method' : "GET",
-        'success' : function (data, stato) {
-
-
-          checkNum(data.response);
-
-          },
-        'error' : function (richiesta, stato, errore) {
-          alert("E' avvenuto un errore. " + errore);
-          }
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function (data, stato) {
+        checkNum(data.response);
+        },
+      error: function (richiesta, stato, errore) {
+        alert("E' avvenuto un errore. " + errore);
+        }
     });
 
     function checkNum(num) {
@@ -29,6 +24,4 @@ $(document).ready(function () {
     }
 
   }); // end on click
-
-
 }); // end ready

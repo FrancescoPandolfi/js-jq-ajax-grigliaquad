989d51597fc3ba09114fb7bea02ac3ea8407c8df
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-30 15:33:49 +0100

    Prova chiamata ajax

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+
+
+$(document).on('click', '.quadrato', function() {
 
 
 
-});
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function (data, stato) {
+console.log(data);
+
+        // if (data <= 5) {
+        //   $(this).addClass('giallo');
+        // } else {
+        //   $(this).addClass('verde');
+        // }
+        },
+      error: function (richiesta, stato, errore) {
+        alert("E' avvenuto un errore. " + errore);
+        }
+    }
+  );
+
+
+
+
+
+
+
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // end ready

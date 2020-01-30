47d6fbf0831c7e2b69e7727368070dcc5f5446c9
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-30 16:15:03 +0100

    Esercizio completato

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
 
 $(document).on('click', '.quadrato', function() {
 
+  var quadrato = this;
 
-
-  $.ajax(
-    {
-      url: "https://flynn.boolean.careers/exercises/api/random/int",
-      method: "GET",
-      success: function (data, stato) {
-console.log(data);
-
-        // if (data <= 5) {
-        //   $(this).addClass('giallo');
-        // } else {
-        //   $(this).addClass('verde');
-        // }
+  $.ajax({
+      'url' : "https://flynn.boolean.careers/exercises/api/random/int",
+      'method' : "GET",
+      'success' : function (data, stato) {
+        checkNum(data.response);
         },
-      error: function (richiesta, stato, errore) {
+      'error' : function (richiesta, stato, errore) {
         alert("E' avvenuto un errore. " + errore);
         }
-    }
-  );
+  });
 
+function checkNum(num) {
+  if (num <= 5) {
+    $(quadrato).addClass('giallo');
+  } else {
+    $(quadrato).addClass('verde');
+  }
+}
 
-
-
-
-
-
-
-// });
-
-
-
-
-
-
-
-
+});
 
 
 

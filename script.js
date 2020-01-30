a2220183ea77653d7546166bc043dfc444014b8f
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-30 16:17:43 +0100

    Fix

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 $(document).ready(function () {
 
 
-$(document).on('click', '.quadrato', function() {
+  $(document).on('click', '.quadrato', function() {
 
-  var quadrato = this;
+    var quadrato = this;
 
-  $.ajax({
-      'url' : "https://flynn.boolean.careers/exercises/api/random/int",
-      'method' : "GET",
-      'success' : function (data, stato) {
-        checkNum(data.response);
-        },
-      'error' : function (richiesta, stato, errore) {
-        alert("E' avvenuto un errore. " + errore);
-        }
-  });
+    $.ajax({
+        'url' : "https://flynn.boolean.careers/exercises/api/random/int",
+        'method' : "GET",
+        'success' : function (data, stato) {
+          checkNum(data.response);
+          },
+        'error' : function (richiesta, stato, errore) {
+          alert("E' avvenuto un errore. " + errore);
+          }
+    });
 
-function checkNum(num) {
-  if (num <= 5) {
-    $(quadrato).addClass('giallo');
-  } else {
-    $(quadrato).addClass('verde');
-  }
-}
+    function checkNum(num) {
+      if (num <= 5) {
+        $(quadrato).addClass('giallo');
+      } else {
+        $(quadrato).addClass('verde');
+      }
+    }
 
-});
-
-
-
-
+  }); // end on click
 
 
 }); // end ready
